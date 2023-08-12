c11807ec518cbe84b4de0ca06c0a980102a0f20f
CarlosLaraFP <info@carloslaraai.com>
2023-08-12 14:56:42 -0700

    Implemented LED roulette

--- src/05-led-roulette/src/main.rs
+++ src/05-led-roulette/src/main.rs
@@ -3,18 +3,42 @@
 #![no_std]
 
 use cortex_m_rt::entry;
-use panic_halt as _;
-use microbit::board::Board;
-use microbit::hal::prelude::*;
+use rtt_target::rtt_init_print;
+use panic_rtt_target as _;
+use microbit::{
+    board::Board,
+    display::blocking::Display,
+    hal::{prelude::*, Timer},
+};
 
 #[entry]
 fn main() -> ! {
-    let mut board = Board::take().unwrap();
+    rtt_init_print!();
 
-    board.display_pins.col1.set_low().unwrap();
-    board.display_pins.row1.set_high().unwrap();
+    let board = Board::take().unwrap();
+    let mut timer = Timer::new(board.TIMER0);
+    let mut display = Display::new(board.display_pins);
+    let mut led_display = [
+        [0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0],
+    ];
+    let roulette = [
+        (0, 0), (0, 1), (0, 2), (0, 3), (0, 4),
+        (1, 4), (2, 4), (3, 4), (4, 4),
+        (4, 3), (4, 2), (4, 1), (4, 0),
+        (3, 0), (2, 0), (1, 0)
+    ];
 
-    loop {}
+    loop {
+        for (i, j) in roulette {
+            let mut copy = led_display.clone();
+            copy[i][j] = 1;
+            display.show(&mut timer, copy, 250);
+        }
+    }
 }
 
 // ** builds and flashes in 1 command **
